bf0f410b4c872ebcc2cdc3dd8fae7aedf5f7933b
PhanNhatLoi <45623505+PhanNhatLoi@users.noreply.github.com>
2024-11-18 17:37:24 +0700

    Config seo (#1)

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://nhatloi-portfolio.vercel.app/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,32 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nhatloi-portfolio.vercel.app/"),
   title: "Nhat Loi Portfolio",
-  description: "Front End Developer(ReactJs, NextJs, React Native)",
+  description:
+    "Portfolio of a Front End Developer specializing in ReactJS, NextJS, and React Native. Explore creative projects and optimized user interface solutions.",
   keywords: [
     "ReactJs",
+    "NodeJs",
+    "NestJs",
     "React native",
     "front-end",
     "front-end developer",
+    "back-end developer",
+    "fullStack",
     "Nhatloi",
     "Phan Nhat Loi",
   ],
   applicationName: "Portfolio",
   viewport: "width=device-width, initial-scale=1",
-  // robots:'',
+  robots: "/robots.txt",
   icons: "/favicon.ico",
+  alternates: {
+    canonical: "/",
+  },
+  verification: {
+    google: "329PkCOAlB8JapopchbQVBteUsSWNRZv9L95pITSESI",
+  },
 };
 
 export default function RootLayout({
